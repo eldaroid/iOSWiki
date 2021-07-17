ddd10e76cf47a53facaf465cd46dd553e8b0021e
Попов Эльдар <eldario10@mail.ru>
2021-07-17 21:57:33 +0300

    Update CleanUpMac.sh

--- Configs/CleanUpMac.sh
+++ Configs/CleanUpMac.sh
@@ -1,4 +1,5 @@
-# To start a project make it executable: chmod +x, then run: sh CleanUpMac.sh
+# To start a project copy content below to file: vim CleanUpMac.sh, make it executable: chmod +x, then run: time sh CleanUpMac.sh
+# &>/dev/null - Смысл вкратце — "Весь вывод указанной команды (вывод в терминале) спихнуть в черную дыру!"
 
 # Colored output in terminal
 Color_Off='\033[0m'       # Text Reset
@@ -26,11 +27,11 @@ oldAvailable=$(df / | tail -1 | awk '{print $4}')
 
 # user cache file
 echo "${Blue}cleaning user cache file from ~/Library/Caches"
-rm -rf ~/Library/Caches/*
+rm -rf ~/Library/Caches/* &>/dev/null
 echo "${Green}done cleaning from ~/Library/Caches"
 # user logs
 echo "${Blue}cleaning user log file from ~/Library/logs"
-rm -rf ~/Library/logs/*
+rm -rf ~/Library/logs/* &>/dev/null
 echo "${Green}done cleaning from ~/Library/logs"
 # user preference log
 echo "${Blue}cleaning user preference logs"
@@ -38,17 +39,17 @@ echo "${Blue}cleaning user preference logs"
 echo "${Green}done cleaning from /Library/Preferences"
 # system caches
 echo "${Blue}cleaning system caches"
-sudo rm -rf /Library/Caches/*
+sudo rm -rf /Library/Caches/* &>/dev/null
 echo "${Green}done cleaning system cache"
 # system logs
 echo "${Blue}cleaning system logs from /Library/logs"
-sudo rm -rf /Library/logs/*
+sudo rm -rf /Library/logs/* &>/dev/null
 echo "${Green}done cleaning from /Library/logs"
 echo "${Blue}cleaning system logs from /var/log"
-sudo rm -rf /var/log/*
+sudo rm -rf /var/log/* &>/dev/null
 echo "${Green}done cleaning from /var/log"
 echo "${Blue}cleaning from /private/var/folders"
-sudo rm -rf /private/var/folders/*
+sudo rm -rf /private/var/folders/* &>/dev/null
 echo "${Green}done cleaning from /private/var/folders"
 # Homebrew
 echo "${Blue}cleaning cache Homebrew"
@@ -59,39 +60,38 @@ brew tap --repair &>/dev/null
 echo "${Green}done cleaning Homebrew"
 # Xcode
 echo "${Blue}cleaning cache Xcode"
-xcrun simctl delete unavailable
-xcodebuild -alltargets clean
-rm -rf ~/Library/Preferences/com.apple.Xcode.plist/*
-rm -rf ~/Library/Developer/Xcode/DerivedData
-rm -rf ~/Library/Developer/CoreSimulator
-rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang/ModuleCache"
+xcrun simctl delete unavailable &>/dev/null
+xcodebuild -alltargets clean &>/dev/null
+rm -rf ~/Library/Preferences/com.apple.Xcode.plist/* &>/dev/null
+rm -rf ~/Library/Developer/Xcode/DerivedData &>/dev/null
+rm -rf ~/Library/Developer/CoreSimulator &>/dev/null
+rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang/ModuleCache" &>/dev/null
 echo "${Green}done cleaning Xcode"
 # Удалите неиспользуемые пакеты и кеши anaconda
 echo "${Blue}cleaning anaconda"
-conda clean --dry-run --all
-conda config --set auto_activate_base false
+conda clean --dry-run --all &>/dev/null
+conda config --set auto_activate_base false &>/dev/null
 echo "${Green}done cleaning anaconda"
 # MS Teams
 echo "${Blue}cleaning MS Teams"
-rm -rf ~/Library/Application\ Support/Microsoft/Teams/Cache/*
-rm -rf ~/Library/Application\ Support/Microsoft/Teams/Service\ Worker/CacheStorage/*
+rm -rf ~/Library/Application\ Support/Microsoft/Teams/Cache/* &>/dev/null
+rm -rf ~/Library/Application\ Support/Microsoft/Teams/Service\ Worker/CacheStorage/* &>/dev/null
 echo "${Green}done cleaning MS Teams"
 # Slack:
 echo "${Blue}cleaning Slack"
-rm -rf ~/Library/Containers/com.tinyspeck.slackmacgap/Data/Library/Application\ Support/Slack/Cache
-rm -rf ~/Library/Containers/com.tinyspeck.slackmacgap/Data/Library/Application\ Support/Slack/Service\ Worker/CacheStorage
+rm -rf ~/Library/Containers/com.tinyspeck.slackmacgap/Data/Library/Application\ Support/Slack/Cache &>/dev/null
+rm -rf ~/Library/Containers/com.tinyspeck.slackmacgap/Data/Library/Application\ Support/Slack/Service\ Worker/CacheStorage &>/dev/null
 echo "${Green}done cleaning slack"
 # ios photo caches
 echo "${Blue}cleaning ios photo caches"
-rm -rf ~/Pictures/iPhoto\ Library/iPod\ Photo\ Cache/*
+rm -rf ~/Pictures/iPhoto\ Library/iPod\ Photo\ Cache/* &>/dev/null
 echo "${Green}done cleaning from ~/Pictures/iPhoto Library/iPod Photo Cache"
 # application caches
 echo "${Blue}cleaning application caches"
-for x in $(ls ~/Library/Containers/) 
-do 
-    echo "cleaning ~/Library/Containers/$x/Data/Library/Caches/"
-    rm -rf ~/Library/Containers/$x/Data/Library/Caches/*
-    echo "done cleaning ~/Library/Containers/$x/Data/Library/Caches"
+for x in $(ls ~/Library/Containers/)
+do
+    rm -rf ~/Library/Containers/$x/Data/Library/Caches/* &>/dev/null
+    echo "${Green}done cleaning ~/Library/Containers/$x/Data/Library/Caches"
 done
 echo "${BGreen}done cleaning for application caches"
 
@@ -99,4 +99,4 @@ newAvailable=$(df / | tail -1 | awk '{print $4}')
 count=$((newAvailable - oldAvailable))
 bytesToHuman $count
 
-echo "${BIPurple}Now you can go to open source mac-cleanup project for the ongoing cleaning mac: ${BIGreen}github.com/fwartner/mac-cleanup"
+echo "${BIPurple}Now you can go to open source mac-cleanup project for the ongoing cleaning mac: ${BIGreen}github.com/fwartner/mac-cleanup${Color_Off}"
